2e9b260b4f65c419f65cb4295da1bed8f92517d0
Cao Xiaoqi <xqcao@thoughtworks.com>
2017-06-28 12:59:09 +0800

    Data fetching both server side and client side

---
+++ pages/about.js
@@ -0,0 +1,7 @@
+import HomeLayout from '../components/HomeLayout';
+
+export default () => (
+  <HomeLayout>
+    <p>This is the about page</p>
+  </HomeLayout>
+);
---
+++ pages/index.js
@@ -0,0 +1,31 @@
+import Link from 'next/link';
+import fetch from 'isomorphic-unfetch';
+import HomeLayout from '../components/HomeLayout';
+
+const Index = (props) => (
+  <HomeLayout>
+    <h1>Batman TV Shows</h1>
+    <ul>
+      {props.shows.map(({show}) => (
+        <li key={show.id}>
+          <Link as={`/p/${show.id}`} href={`/post?id=${show.id}`}>
+            <a>{show.name}</a>
+          </Link>
+        </li>
+      ))}
+    </ul>
+  </HomeLayout>
+);
+
+Index.getInitialProps = async function() {
+  const res = await fetch('https://api.tvmaze.com/search/shows?q=batman')
+  const data = await res.json()
+
+  console.log(`Show data fetched. Count: ${data.length}`)
+
+  return {
+    shows: data
+  }
+}
+
+export default Index;
